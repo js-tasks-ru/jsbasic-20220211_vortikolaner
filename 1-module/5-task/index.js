de8232248ff0559e3617b4b1514f4b2b5bfe5923
Mikhail <vortikolaner@gmail.com>
2022-02-18 17:39:46 +0300

    strLength

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,9 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if( str.length > maxlength){
+ 
+    str = str.substr(0, maxlength );
+    str += String.fromCharCode(8230);
+    return console.log(str); 
+  }
+  console.log(str);
 }
