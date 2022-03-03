ea8623167011b79fb7b56cb90656a69665d1a3b5
Mikhail <vortikolaner@gmail.com>
2022-03-03 15:28:27 +0300

    3modul5task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,10 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('-');
+    let arr2 = [];
+    let str2 = arr.shift();
+    for(let key of arr){
+      arr2.push(key[0].toUpperCase() + key.slice(1))
+    }
+    arr2.unshift(str2)
+    return arr2.join('')
 }
--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,23 @@
-function showSalary(users, age) {
-  // ваш код...
+let user1 = {
+  "balance": "$1,825.65",
+  "picture": "https://placehold.it/32x32",
+  "age": 21,
+  "name": "Golden Branch",
+  "gender": "male",
+  "greeting": "Hello, Golden Branch! You have 7 unread messages.",
+  "favouriteFruit": "banana"
+};
+
+let users = [user1];
+
+function showSalary(users, age){
+  let arr = [];
+   users.forEach( item =>{
+    if(item.age <= age){
+      arr.push(`${item.name}, ${item.balance}`);
+    }
+  })
+    return arr.join('\n');
 }
+
+console.log( showSalary(users, 25) )
